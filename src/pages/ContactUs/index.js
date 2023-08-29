68cdde89ceb41eeebf03ecc951d8dc41809996fe
AkashPatil22 <patilakash3680@gmail.com>
2023-08-29 17:31:13 +0530

    Front End Added

---
+++ src/pages/ContactUs/index.js
@@ -0,0 +1,31 @@
+import Footer from '../../components/Footer'
+import HeaderUser from '../../components/HeaderUser'
+
+const ContactUs = () => {
+
+    return(
+        <div>
+            <HeaderUser/>
+            <br/>
+            <br/>
+            <h3>Send Your Feedback at</h3>
+            <br/>
+            <div>Orbgen Technologies pvt.ltd, Sree Teja Tower, First Floor, New no 81, Old no 130, Nelson Manickam Road, Chennai-600 029</div>
+            <br/>
+            <hr/>
+            <br/>
+            <h3>Support Services (24x7)</h3>
+            <br/>
+            <a href="support@filmticket.com"> support@filmticket.com</a>
+            <br/>
+            <br/>
+            <a href="+91 8438584385">+91 8438584385</a>
+            <br/>
+            <hr/>
+            <br/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default ContactUs
